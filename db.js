7ae041bd61c384262ed5249c8853637ed8e37586
Pathan Sumer Khan <ksumer597@gmail.com>
2024-02-04 02:44:13 +0530

    update db file

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //Define mongoDB connection URL
 const mongoURl = 'mongodb://localhost:27017/hotelDB'
-                 //where hotelDB -->   own DB name.
+  //where hotelDB -->   own DB name.
 
 mongoose.connect(mongoURl, {
   useNewUrlParser:true,
@@ -14,7 +14,9 @@ mongoose.connect(mongoURl, {
 // helps to estabilish bridge betwn MongoDB & Node.js
 const db = mongoose.connection; 
 
+
 // .on define event listeners for DB connection
+// can console custom msg to get db status.
 
 db.on('connected',() => {
   console.log('COnnected to MongoDB server successfully.')
